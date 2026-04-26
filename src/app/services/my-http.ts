755ed37c2e92fddbfd7478277b775434480cd885
David <coleman887@gmail.com>
2026-04-26 16:59:04 +0100

    Built out MyHttpService, added the 5 methods needed to handle the API calls

--- src/app/services/my-http.ts
+++ src/app/services/my-http.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'
 @Injectable({
   providedIn: 'root',
 })
-export class MyHttp {
-  
-}
+export class MyHttpService {
+
+  apiKey: string = "20227f3ffe578d2206950f3e07386265";
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getTrending(): Observable<any> {
+    return this.http.get("https://api.themoviedb.org/3/trending/movie/day?api_key=" + this.apiKey);
+
+    }
+
+  searchMovies(query: string): Observable<any> {
+    return this.http.get("https://api.themoviedb.org/3/search/movie?query=" + query + "&api_key=" + this.apiKey);
+  }
+
+  getMovieCredits(movieId: number): Observable<any> {
+    return this.http.get("https://api.themoviedb.org/3/movie/" + movieId + "/credits?api_key=" + this.apiKey);
+  }
+
+  getPersonDetails(personId: number): Observable<any> {
+    return this.http.get("https://api.themoviedb.org/3/person/" + personId + "?api_key=" + this.apiKey);
+  }
+
+  getPersonMovieCredits(personId: number): Observable<any> {
+    return this.http.get("https://api.themoviedb.org/3/person/" + personId + "/movie_credits?api_key=" + this.apiKey);
+  }
+  }
+
