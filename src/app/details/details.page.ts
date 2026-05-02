941b5b542ac64d1399555e5efb6e7606693a7f60
David <coleman887@gmail.com>
2026-05-02 18:54:09 +0100

    Cast/Crew details.ts page built out, started on the .html, reduced all image sizes for readability

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonIcon, IonToolbar, IonCard } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { heart, home, logoApple, settingsSharp, star} from 'ionicons/icons';
+import { MyHttpService } from '../services/my-http';
+import { DataService } from '../services/data';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonIcon, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard]
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  personDetails: any = "";
+  personMovieCredits: any[] = [];
+
+  constructor(public router: Router, private myhttp: MyHttpService, public data: DataService) { 
+
+  }
+
+  getPersonDetails(id: number) {
+    this.myhttp.getPersonDetails(id).subscribe((data: any) => {
+      this.personDetails = data;
+    })
+  }
+
+  getPersonMovieCredits(id: number) {
+    this.myhttp.getPersonMovieCredits(id).subscribe((data: any) => {
+      this.personMovieCredits = data.cast;
+    })
+  }
+
+
 
   ngOnInit() {
+    let id = this.data.selectedPersonId;
+    this.getPersonDetails(id);
+    this.getPersonMovieCredits(id);
   }
 
 }
