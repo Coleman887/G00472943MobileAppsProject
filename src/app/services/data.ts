5d68b4e0620497ec5c7a0b5a80d8e62939a4539d
David <coleman887@gmail.com>
2026-05-03 17:20:54 +0100

    Added ionic storage, implemented it to the existing methods in Dataservice so that added or removed favourites are stored even after the application has been closed. UI cleanup and some extra functionality next

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular'
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    const storage = await this.storage.create();
+  }
+
+  async set(key: string, value: any) {
+    await this.storage.set(key, value);
+  }
 
   selectedMovieTitle: string = "";
   selectedMovieId: number = 0;
@@ -13,23 +26,29 @@ export class DataService {
   selectedMoviePoster: string = "";
 
 // This method will 'push' the selected movie to the favouritesList array.
-addFavourite(movie: any) { 
+async addFavourite(movie: any) { 
     this.favouritesList.push(movie);
+    await this.storage.set('favourites', this.favouritesList);
   }
 
 // Using this method to cleanly loop through the favouritesList array, and filters the array keeping the movies that don't match the id.
-removeFavourite(movie: any) { 
+async removeFavourite(movie: any) { 
     this.favouritesList = this.favouritesList.filter(item => item.id !== movie.id); 
+    await this.storage.set('favourites', this.favouritesList);
   }
 
 // Method with regular for loop, returning a boolean value depending on if the movie in the favouritesList at the [i]th element matches the movie currenty being displayed.
-  isFavourite(movieId: number) {
+    isFavourite(movieId: number) {
     for (let i = 0; i < this.favouritesList.length; i++) {
       if (this.favouritesList[i].id == movieId) {
           return true;
       }
     }
     return false;
+  }
+
+  async storedFavourites() {
+    this.favouritesList = await this.storage.get('favourites');
   }
 }
 
